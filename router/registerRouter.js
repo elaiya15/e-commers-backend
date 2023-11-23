b59d79b0c2454a6f85495343dbab3f6d20676d35
elaiya15 <elaiyakutty143@gmail.com>
2023-11-23 15:11:28 +0530

    update

--- router/registerRouter.js
+++ router/registerRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const register = require("../modules/registerModule");
+const register = require ("../modules/registerModule");
 
 const router = express.Router();
 
