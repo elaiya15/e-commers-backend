ae967012838a1c6d754040345f10ce84f4108bdd
elaiya15 <elaiyakutty143@gmail.com>
2023-11-23 13:41:20 +0530

    vercel

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/register", registerRouter);
 app.use("/", auth.authenticateUser);
 
 // app.listen(process.env.PORT);
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT||5000,()=>{
     console.log("Server is running on port",process.env.PORT)
 });
 
