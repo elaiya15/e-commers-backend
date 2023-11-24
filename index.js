60abb49e57ff9773802ccd4a09b8966dd7734cdf
elaiya15 <elaiyakutty143@gmail.com>
2023-11-24 13:16:55 +0530

    done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/reg/v1", registerRouter);
+app.use("/", registerRouter);
 
 app.get('/', (req, res) => {
  res.send({server:" runing good"});
